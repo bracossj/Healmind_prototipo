ce4a62833259f71529b978aaec5ce03cc27f1511
unknown <luisfelipece02@gmail.com>
2023-10-21 15:24:03 -0500

    updated (home & buttons)

--- frontend/src/pages/auth.jsx
+++ frontend/src/pages/auth.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import '../styles/form.css';
 import 'animate.css';
+import { BiArrowBack } from 'react-icons/bi';
 
 function LoginPage({ switchToRegister }) {
     const { register, handleSubmit } = useForm();
@@ -12,8 +13,8 @@ function LoginPage({ switchToRegister }) {
 
     return (
         <div className='auth'>
-
             <form className="form animate__animated animate__backInDown" onSubmit={handleSubmit(onSubmit)}>
+                <a href="/"></a>
                 <h1 className="titleform">Iniciar sesión</h1>
 
                 <div className="inputform">
@@ -27,13 +28,14 @@ function LoginPage({ switchToRegister }) {
                 </div>
 
                 <div className="buttons">
+                    <a className="button" href="/"><BiArrowBack /></a>
+
                     <a href="#" onClick={switchToRegister} className='button'>No tienes cuenta?</a>
 
                     <button type="submit" className="button">Iniciar sesión</button>
                 </div>
             </form>
         </div>
-
     );
 }
 
@@ -75,6 +77,8 @@ function RegisterPage({ switchToLogin }) {
                 </div>
 
                 <div className="buttons">
+                    <a className="button" href="/"><BiArrowBack /></a>
+
                     <a href="#" onClick={switchToLogin} className='button'>Tienes cuenta?</a>
 
                     <button type="submit" className="button">Registrarse</button>
--- frontend/src/pages/home_page.jsx
+++ frontend/src/pages/home_page.jsx
@@ -6,10 +6,10 @@ import { BiLogIn } from "react-icons/bi";
 function HomePage() {
 
     return (
-        <div className="home">
+        <div>
             <nav className="navbarhome">
                 <div className="title_logo">
-                    <h1 className="titlehome">HealtMind</h1>
+                    <h1 className="titlehome">HealMind</h1>
                     <img src={logo} alt="" className="logonav" />
                 </div>
 
@@ -23,7 +23,45 @@ function HomePage() {
                     <a href="/auth">Ingresar <BiLogIn size="20px" /></a>
                 </div>
             </nav>
-            <div></div>
+            <div className="content">
+                <section className="sec">
+                    <h1 className="tittleshome">¿Que es la salud mental?</h1>
+                    <h1 className="infoshome">
+                        La salud mental es un estado de bienestar que permite a las personas hacer frente al estrés de la vida, desarrollar sus habilidades, aprender y trabajar bien, y contribuir a su comunidad.
+                        En los estudiantes universitarios, la salud mental es especialmente importante, ya que se enfrentan a una serie de desafíos, como la presión académica, las relaciones sociales y la incertidumbre sobre el futuro.
+                    </h1>
+                </section>
+                <section className="sec">
+                    <h1 className="tittleshome">¿Por qué es importante?</h1>
+                    <h1 className="infoshome">
+                        La salud mental es importante para los estudiantes universitarios por las siguientes razones:
+                    </h1>
+                </section>
+                <section className="sec">
+                    <div className="section_disorders">
+                        <div className="disorders">
+                            <h1 className="tittleshome">Estrés</h1>
+                            <h1 className="infoshome">
+                                El estrés es común entre los estudiantes debido a la presión académica y social. Puede afectar el rendimiento académico y la calidad de vida. Ofrecemos apoyo y consejos para manejar el estrés.
+                            </h1>
+                        </div>
+
+                        <div className="disorders">
+                            <h1 className="tittleshome">Ansiedad</h1>
+                            <h1 className="infoshome">
+                                La ansiedad puede dificultar la concentración y la toma de decisiones. Nuestros servicios incluyen terapias y estrategias para reducir la ansiedad.
+                            </h1>
+                        </div>
+
+                        <div className="disorders">
+                            <h1 className="tittleshome">Depresión</h1>
+                            <h1 className="infoshome">
+                                La depresión puede ser debilitante y afectar la calidad de vida. Te ofrecemos recursos y apoyo para enfrentar la depresión de manera efectiva.
+                            </h1>
+                        </div>
+                    </div>
+                </section>
+            </div>
         </div>
     )
 }
