709170c0963e19cf95ed0ac53db97ffaacf2c696
unknown <luisfelipece02@gmail.com>
2023-11-02 09:44:29 -0500

    validates

---
+++ src/schemas/auth.schema.js
@@ -0,0 +1,32 @@
+import { z } from 'zod';
+
+export const registerSchema = z.object({
+    name: z.string({
+        required_error: "Nombre requerido"
+    }),
+    lastname: z.string({
+        required_error: "Apellido requerido"
+    }),
+    age: z.number({
+        required_error: "Edad requerida"
+    }),
+    email: z.string({
+        required_error: "Correo requerido"
+    }).email({
+        required_error: "Correo invalido"
+    }),
+    password: z.string({
+        required_error: "Contraseña requerida"
+    })
+})
+
+export const loginSchema = z.object({
+    email: z.string({
+        required_error: "Correo requerido"
+    }).email({
+        required_error: "Correo invalido"
+    }),
+    password: z.string({
+        required_error: "Contraseña requerida"
+    })
+})
